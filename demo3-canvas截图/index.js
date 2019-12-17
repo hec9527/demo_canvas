dcb210ed48150404c105bc31472eb566227902cf
hec <chun.he@100credit.com>
2019-12-17 15:07:03 +0800

    重构canvas截图demo

---
+++ demo3-canvas截图/index.js
@@ -0,0 +1,264 @@
+/**
+ * 绘制钟表
+ */
+class DrawClock {
+    constructor() {
+        this.canvas =
+            document.getElementById('canvas') ||
+            function() {
+                const el = document.createElement('canvas');
+                el.style.width = '800px';
+                el.style.height = '450px';
+                el.style.position = 'absolute';
+                el.style.margin = 'auto';
+                el.style.top = 0;
+                el.style.bottom = 0;
+                el.style.left = 0;
+                el.style.right = 0;
+                el.width = 800;
+                el.height = 450;
+                el.style.background = 'rgba(0, 0, 0, 0.3';
+                return el;
+            };
+        this.ctx = this.canvas.getContext('2d');
+        this.FONT_HEIGHT = 20; // 字体高度  字体大小
+        this.MARGIN = 35; // 距离边框的距离
+        this.NUMBER_SPACING = -30; // 数字距离外框的距离
+        this.RADIUS = this.canvas.height / 2 - this.MARGIN; // 外圆盘的半径
+        this.NUMBER_RADIUS = this.RADIUS + this.NUMBER_SPACING; // 绘制数字的半径
+        this.POINTERS = {
+            hour: {
+                length: (this.RADIUS / 9) * 4,
+                width: 4
+            },
+            minute: {
+                length: (this.RADIUS / 3) * 2,
+                width: 2
+            },
+            seconds: {
+                length: (this.RADIUS / 9) * 7,
+                width: 1
+            }
+        };
+        this.ctx.font = this.FONT_HEIGHT + 'px Arial';
+        this.ctx.fillStyle = '#fff';
+        this.ctx.strokeStyle = '#fff';
+        setTimeout(() => this.draw(), 0);
+    }
+
+    // 绘制外圈圆环
+    drawCircle() {
+        this.ctx.save();
+        this.ctx.lineWidth = 8;
+        this.ctx.beginPath();
+        this.ctx.arc(
+            this.canvas.width / 2,
+            this.canvas.height / 2,
+            this.RADIUS,
+            0,
+            Math.PI * 2,
+            true
+        );
+        this.ctx.closePath();
+        this.ctx.stroke();
+        this.ctx.restore();
+    }
+
+    // 绘制所有刻度
+    drawTags() {
+        // 只要是非连续路径绘制，都要记得都要执行一句context.beginPath()
+        // 然后使用context.endPath() 结束路径
+        // 否则会以最后一次的绘制样式为准
+        this.ctx.save();
+        for (let i = 0; i < 60; i++) {
+            this.ctx.beginPath();
+            this.ctx.lineWidth = 1;
+
+            const angle = (Math.PI / 30) * i;
+            let lineLength = 10;
+
+            if (i % 15 === 0) {
+                // 3 6 9  12小时绘制粗长的直线
+                this.ctx.lineWidth = 5;
+                lineLength = 20;
+            } else if (i % 5 === 0) {
+                // 冯5的倍数，绘制次级线条
+                this.ctx.lineWidth = 3;
+                lineLength = 15;
+            }
+
+            this.ctx.moveTo(
+                this.canvas.width / 2 + Math.cos(angle) * this.RADIUS,
+                this.canvas.height / 2 + Math.sin(angle) * this.RADIUS
+            );
+            this.ctx.lineTo(
+                this.canvas.width / 2 + Math.cos(angle) * (this.RADIUS - lineLength),
+                this.canvas.height / 2 + Math.sin(angle) * (this.RADIUS - lineLength)
+            );
+            this.ctx.closePath();
+            this.ctx.stroke();
+        }
+        this.ctx.restore();
+    }
+
+    // 绘制数字
+    drawNumbers() {
+        const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+        let angle = 0;
+        let numberWidth = 0;
+
+        numbers.forEach(num => {
+            angle = (Math.PI / 6) * (num - 3);
+            numberWidth = this.ctx.measureText(num).width;
+            this.ctx.fillText(
+                num,
+                this.canvas.width / 2 + Math.cos(angle) * this.NUMBER_RADIUS - numberWidth / 2,
+                this.canvas.height / 2 + Math.sin(angle) * this.NUMBER_RADIUS + this.FONT_HEIGHT / 3
+            );
+        });
+    }
+
+    // 绘制中间的原点
+    drawCenter() {
+        this.ctx.beginPath();
+        this.ctx.arc(
+            (this.canvas.width / 2) | 0,
+            (this.canvas.height / 2) | 0,
+            5,
+            0,
+            Math.PI * 2,
+            true
+        );
+        this.ctx.closePath();
+        this.ctx.fill();
+    }
+
+    // 绘制指针
+    drawHand(/** number: 刻度/360 */ ioc, pType) {
+        const angle = Math.PI * 2 * (ioc / 60) - Math.PI / 2; // 坐标系朝右为0度，3时
+        const POINTER = this.POINTERS[pType];
+
+        this.ctx.save();
+        this.ctx.lineWidth = POINTER.width;
+        this.ctx.moveTo(this.canvas.width / 2, this.canvas.height / 2);
+        this.ctx.lineTo(
+            (this.canvas.width / 2 + Math.cos(angle) * POINTER.length) | 0,
+            (this.canvas.height / 2 + Math.sin(angle) * POINTER.length) | 0
+        );
+        this.ctx.stroke();
+        this.ctx.restore();
+    }
+
+    // 绘制所有指针
+    drawHands() {
+        const date = new Date();
+        const hour = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+
+        this.drawHand(hour * 5 + (date.getMinutes() / 60) * 5, 'hour'); // 绘制时针
+        this.drawHand(date.getMinutes(), 'minute'); // 绘制分针
+        this.drawHand(date.getSeconds(), 'seconds'); // 绘制秒针
+    }
+
+    // 绘制右侧数字
+    drwaDateNumber() {
+        const date = new Date();
+        this.ctx.save();
+        this.ctx.font = 40 + 'px Arial';
+        const hour = `${date.getHours()} : ${
+            date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
+        } : ${date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()}`;
+        const hourSize = this.ctx.measureText(hour);
+        this.ctx.fillText(
+            hour,
+            (this.canvas.width / 2 - hourSize.width / 2) | 0,
+            this.canvas.height / 2 - (this.RADIUS / 9) * 2
+        );
+
+        this.ctx.font = 15 + 'px Arial';
+        const weekDay = `星期${this.mapWeekDay(date.getDay())}`;
+        const weekDaySize = this.ctx.measureText(weekDay).width;
+        this.ctx.fillText(
+            weekDay,
+            (this.canvas.width / 2 - weekDaySize / 2) | 0,
+            this.canvas.height / 2 + (this.RADIUS / 8) * 3
+        );
+
+        this.ctx.restore();
+    }
+
+    mapWeekDay(num) {
+        const days = ['日', '一', '二', '三', '四', '五', '六'];
+        return days[num];
+    }
+
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.drawCircle();
+        this.drawTags();
+        this.drawNumbers();
+        this.drawCenter();
+        this.drawHands();
+        this.drwaDateNumber();
+
+        requestAnimationFrame(() => {
+            this.draw();
+        });
+    }
+}
+
+// ------------------------------  以上代码为demo1的代码     ------------------------------
+
+// 以下代码为   获取canvas中的图片 并且截图保存到本地，或者再次绘制到页面
+
+class CanvasSnap {
+    constructor() {
+        const drawClock = new DrawClock();
+
+        this.canvas = drawClock.canvas;
+        this.ctx = drawClock.ctx;
+        this.btnBlob = document.getElementById('blob');
+        this.btnDataURL = document.getElementById('dataURL');
+
+        // 使用 toBlob API 获取屏幕截图  这个方法属于异步调用，使用会掉函数处理
+        // 会掉函数第一个参数为 一个blob对象
+        this.btnBlob.addEventListener(
+            'click',
+            e => {
+                // void canvas.toBlob(callback, mimeType, quality);
+                this.canvas.toBlob(
+                    blob => {
+                        // 必须使用这个方法创建一个短链接才能在 src/href 属性中使用
+                        // 过多的资源可以使用 URL.revokeObjectURL 删除引用
+                        const ulr = URL.createObjectURL(blob);
+                        const el = document.createElement('a');
+
+                        el.download = 'canvas截图-toBlob';
+                        el.href = ulr;
+                        el.click();
+                    },
+                    'image/png',
+                    96
+                );
+            },
+            false
+        );
+
+        this.btnDataURL.addEventListener(
+            'click',
+            e => {
+                // canvas.toDataURL([mimeType, quality]);
+                const imgData = this.canvas.toDataURL('image/png', 96);
+                const el = document.createElement('a');
+
+                console.log(1);
+
+                el.download = 'canvas截图-toDataURL';
+                el.href = imgData;
+                el.click();
+            },
+            false
+        );
+    }
+}
+
+new CanvasSnap();
