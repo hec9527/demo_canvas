2961b3496a3b3703dc68285feb0c2d0abc6250f2
hec <chun.he@100credit.com>
2020-01-03 17:19:13 +0800

    将坦克大战合并到项目

---
+++ demo11-坦克大战/index.js
@@ -0,0 +1,99 @@
+/**
+ * @author     hec9527
+ * @time       2020-1-3
+ * @change     2020-1-3
+ * @description
+ *
+ *      1, 自由软件协议
+ *      2，本游戏采用HTML、CSS、JS制作
+ *      3，在游戏逻辑部分使用ES6的语法，需要一定的canvas功底
+ *
+ */
+
+/**
+ * 键盘类
+ */
+class KeyBorad {
+    constructor(word) {
+        this.word = word;
+        this.BLOCK = this.word.game.KEYBORAD_BLOCK = false;
+
+        // ! TODO 双线发展，后期测试 key 和 keyCode 究竟那个更加方便合适
+        this.keyCode = new Set();
+        this.keys = new Set();
+
+        // 键盘   --  按下
+        window.addEventListener('keydown', e => {
+            if (!this.BLOCK) {
+                this.keyCode.add(e.keyCode);
+                this.keys.add(e.key);
+            }
+        });
+
+        // 键盘  -- 抬起
+        window.addEventListener('keyup', e => {
+            this.keyCode.delete(e.keyCode);
+            this.key.delete(e.key);
+        });
+    }
+
+    hasKey(key) {
+        return this.keys.has(key);
+    }
+
+    hasKeyCode(code) {
+        return this.keyCode.has(code);
+    }
+
+    clear() {
+        this.keyCode.clear();
+        this.keys.clear();
+    }
+}
+
+/**
+ * 图片加载类
+ * 负责加载并且处理图像
+ */
+class Image {
+    constructor(word) {
+        this.word = word;
+    }
+}
+
+/**
+ * 游戏类
+ * 用于存放游戏中的运行时参数
+ */
+class Game {
+    constructor(word) {
+        this.word = word;
+
+        // 键盘锁定
+        this.KEYBORAD_BLOCK = false;
+    }
+}
+
+/**
+ * 世界类
+ * 游戏的核心类，所有的处理都包含在这个类里面
+ * 游戏中的顶级类
+ */
+class Word {
+    constructor() {
+        // 世界基础属性
+        this.canvas = document.getElementById('canvas');
+        this.ctx = this.canvas.getContext('2d');
+        this.width = this.canvas.width = this.canvas.offsetWidth;
+        this.height = this.canvas.height = this.canvas.offsetHeight;
+
+        // 世界元素属性
+        this.items = [];
+
+        // 世界其它属性
+        this.keyBorad = new KeyBorad(this);
+    }
+}
+
+// !后期删除对外接口，采用闭包的方式使用
+const word = new Word();
